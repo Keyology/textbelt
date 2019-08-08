286b239791e47291349a3b4ac8713e5d70e282e4
Keyology <keonimurray45@gmail.com>
2019-08-08 04:43:27 -0700

    app is ready for production

--- server/app.js
+++ server/app.js
@@ -1,34 +1,46 @@
-const express = require('express');
+const express = require("express");
 
-const carriers = require('../lib/carriers.js');
-const providers = require('../lib/providers.js');
-const text = require('../lib/text');
-
+const carriers = require("../lib/carriers.js");
+const providers = require("../lib/providers.js");
+const text = require("../lib/text");
 const app = express();
+const Sentry = require("@sentry/node");
+
+// Sentry.init({
+//   dsn: "https://1a07780f3c74402fadff8bdad7480576@sentry.io/1525166",
+//   environment: "Production"
+// });
 
 // Express config
+// app.use(Sentry.Handlers.requestHandler());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   // Enable CORS so sites can use the API directly in JS.
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // Enable log messages when sending texts.
 text.config({
-  debugEnabled: true,
+  debugEnabled: true
 });
 
 // App helper functions.
 function stripPhone(phone) {
-  return (`${phone}`).replace(/\D/g, '');
+  return `${phone}`.replace(/\D/g, "");
 }
 
 function textRequestHandler(req, res, number, carrier, region) {
   if (!number || !req.body.message) {
-    res.send({ success: false, message: 'Number and message parameters are required.' });
+    res.send({
+      success: false,
+      message: "Number and message parameters are required."
+    });
     return;
   }
 
@@ -38,26 +50,29 @@ function textRequestHandler(req, res, number, carrier, region) {
     if (carriers[carrierKey] == null) {
       res.send({
         success: false,
-        message: `Carrier ${carrier} not supported! POST getcarriers=1 to `
-                                                               + 'get a list of supported carriers',
+        message:
+          `Carrier ${carrier} not supported! POST getcarriers=1 to ` +
+          "get a list of supported carriers"
       });
       return;
     }
   }
 
   let { message } = req.body;
-  if (message.indexOf(':') > -1) {
+  if (message.indexOf(":") > -1) {
     // Handle problem with vtext where message would not get sent properly if it
     // contains a colon.
     message = ` ${message}`;
   }
 
   // Time to actually send the message
-  text.send(number, message, carrierKey, region, (err) => {
+  text.send(number, message, carrierKey, region, err => {
     if (err) {
       res.send({
         success: false,
-        message: `Communication with SMS gateway failed. Did you configure mail transport in lib/config.js?  Error message: '${err.message}'`,
+        message: `Communication with SMS gateway failed. Did you configure mail transport in lib/config.js?  Error message: '${
+          err.message
+        }'`
       });
     } else {
       res.send({ success: true });
@@ -66,41 +81,57 @@ function textRequestHandler(req, res, number, carrier, region) {
 }
 
 // App routes
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("I'm online!");
 });
 
-app.get('/providers/:region', (req, res) => {
+app.get("/providers/:region", (req, res) => {
   // Utility function, just to check the providers currently loaded
   res.send(providers[req.params.region]);
 });
 
-app.post('/text', (req, res) => {
-  if (req.body.getcarriers != null
-      && (req.body.getcarriers === '1'
-       || req.body.getcarriers.toLowerCase() === 'true')) {
+app.post("/text", (req, res) => {
+  if (
+    req.body.getcarriers != null &&
+    (req.body.getcarriers === "1" ||
+      req.body.getcarriers.toLowerCase() === "true")
+  ) {
     res.send({ success: true, carriers: Object.keys(carriers).sort() });
     return;
   }
   const number = stripPhone(req.body.number);
   if (number.length < 9 || number.length > 10) {
-    res.send({ success: false, message: 'Invalid phone number.' });
+    res.send({ success: false, message: "Invalid phone number." });
     return;
   }
-  textRequestHandler(req, res, number, req.body.carrier, 'us');
+  textRequestHandler(req, res, number, req.body.carrier, "us");
 });
 
-app.post('/canada', (req, res) => {
-  textRequestHandler(req, res, stripPhone(req.body.number), req.body.carrier, 'canada');
+app.post("/canada", (req, res) => {
+  textRequestHandler(
+    req,
+    res,
+    stripPhone(req.body.number),
+    req.body.carrier,
+    "canada"
+  );
 });
 
-app.post('/intl', (req, res) => {
-  textRequestHandler(req, res, stripPhone(req.body.number), req.body.carrier, 'intl');
+app.post("/intl", (req, res) => {
+  textRequestHandler(
+    req,
+    res,
+    stripPhone(req.body.number),
+    req.body.carrier,
+    "intl"
+  );
 });
+
+// app.use(Sentry.Handlers.errorHandler());
 
 // Start server
 const port = process.env.PORT || 9090;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log('Listening on', port);
+  console.log("Listening on", port);
 });
